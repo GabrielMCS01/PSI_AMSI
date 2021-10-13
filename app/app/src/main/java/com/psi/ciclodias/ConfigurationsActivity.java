9519f2b2ad3c99080bf6367d95f3e32bcc72467a
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-10-13 11:48:28 +0100

    Criação das activitys e fragments

---
+++ app/app/src/main/java/com/psi/ciclodias/ConfigurationsActivity.java
@@ -0,0 +1,14 @@
+package com.psi.ciclodias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigurationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurations);
+    }
+}
---
+++ app/app/src/main/java/com/psi/ciclodias/StartTrainingActivity.java
@@ -0,0 +1,14 @@
+package com.psi.ciclodias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_training);
+    }
+}
