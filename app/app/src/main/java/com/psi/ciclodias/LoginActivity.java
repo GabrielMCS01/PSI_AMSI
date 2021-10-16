acdf0d32a953211c27b2ead2c094cb8bec662322
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-10-16 20:48:26 +0100

    Layout Login já redireciona, concluido o registo login falta corrigir bug no verificar password e email

--- app/app/src/main/java/com/psi/ciclodias/LoginActivity.java
+++ app/app/src/main/java/com/psi/ciclodias/LoginActivity.java
@@ -2,13 +2,48 @@ package com.psi.ciclodias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btEntrar, btRegisto;
+    private EditText tvEmail, tvPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btEntrar = findViewById(R.id.btEntrar);
+        btRegisto = findViewById(R.id.btRegistar);
+        tvEmail = findViewById(R.id.etLoginEmail);
+        tvPassword = findViewById(R.id.etLoginPassword);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+
+                // Recebe os itens das editViews
+                String email = tvEmail.getText().toString();
+                String password = tvPassword.getText().toString();
+
+                // fazer a verificação se o login está correto
+
+                startActivity(intent);
+            }
+        });
+
+        btRegisto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegistryActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/app/src/main/java/com/psi/ciclodias/RegistryActivity.java
+++ app/app/src/main/java/com/psi/ciclodias/RegistryActivity.java
@@ -2,13 +2,54 @@ package com.psi.ciclodias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistryActivity extends AppCompatActivity {
+    private EditText etPrimeiroNome, etUltimoNome, etEmail, etConfirmarEmail, etPassword, etConfirmarPassword;
+    private Button btRegistarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registry);
+
+        etPrimeiroNome = findViewById(R.id.etPrimeiroNome);
+        etUltimoNome = findViewById(R.id.etUltimoNome);
+        etEmail = findViewById(R.id.etRegistoEmail);
+        etConfirmarEmail = findViewById(R.id.etConfirmarEmail);
+        etPassword = findViewById(R.id.etRegistoPassword);
+        etConfirmarPassword = findViewById(R.id.etConfirmarPassword);
+        btRegistarse = findViewById(R.id.btRegistarse);
+
+        btRegistarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+                String primeiroNome = etPrimeiroNome.getText().toString();
+                String ultimoNome = etUltimoNome.getText().toString();
+                String email = etEmail.getText().toString();
+                String confirmarEmail = etConfirmarEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String confirmarPassword = etConfirmarPassword.getText().toString();
+
+                if (email == confirmarEmail && password == confirmarPassword){
+                    // Verificar o resto dos dados
+                    // Enviar dados para a base de dados
+
+                    startActivity(intent);
+                }
+                else {
+
+                }
+            }
+        });
+
     }
+
+
 }
