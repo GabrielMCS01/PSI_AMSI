8cd189d23c6579b6ad844c866ff999224e8698c6
Gabriel Silva <gabrielcamposilva@sapo.pt>
2021-11-29 11:51:53 +0000

    Avanços na DB

---
+++ app/app/src/main/java/com/psi/ciclodias/model/Ciclismo.java
@@ -0,0 +1,115 @@
+package com.psi.ciclodias.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Ciclismo {
+    private long id, user_id_ciclismo;
+    private String nome_percurso, velocidade_grafico, rota;
+    private float distancia, velocidade_media, velocidade_maxima;
+    private String data_treino, duracao;
+
+    public Ciclismo(String nome_percurso, String duracao, float distancia, float velocidade_media, float velocidade_maxima, String velocidade_grafico, String rota) {
+        this.nome_percurso = nome_percurso;
+        this.velocidade_grafico = velocidade_grafico;
+        this.rota = rota;
+        this.distancia = distancia;
+        this.velocidade_media = velocidade_media;
+        this.velocidade_maxima = velocidade_maxima;
+        this.duracao = duracao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUser_id_ciclismo() {
+        return user_id_ciclismo;
+    }
+
+    public void setUser_id_ciclismo(long user_id_ciclismo) {
+        this.user_id_ciclismo = user_id_ciclismo;
+    }
+
+    public String getNome_percurso() {
+        return nome_percurso;
+    }
+
+    public void setNome_percurso(String nome_percurso) {
+        this.nome_percurso = nome_percurso;
+    }
+
+    public String getVelocidade_grafico() {
+        return velocidade_grafico;
+    }
+
+    public void setVelocidade_grafico(String velocidade_grafico) {
+        this.velocidade_grafico = velocidade_grafico;
+    }
+
+    public String getRota() {
+        return rota;
+    }
+
+    public void setRota(String rota) {
+        this.rota = rota;
+    }
+
+    public float getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(float distancia) {
+        this.distancia = distancia;
+    }
+
+    public float getVelocidade_media() {
+        return velocidade_media;
+    }
+
+    public void setVelocidade_media(float velocidade_media) {
+        this.velocidade_media = velocidade_media;
+    }
+
+    public float getVelocidade_maxima() {
+        return velocidade_maxima;
+    }
+
+    public void setVelocidade_maxima(float velocidade_maxima) {
+        this.velocidade_maxima = velocidade_maxima;
+    }
+
+    public String getData_treino() {
+        return data_treino;
+    }
+
+    public void setData_treino(String data_treino) {
+        this.data_treino = data_treino;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nome_percurso='" + nome_percurso + '\'' +
+                ", velocidade_grafico='" + velocidade_grafico + '\'' +
+                ", rota='" + rota + '\'' +
+                ", distancia=" + distancia +
+                ", velocidade_media=" + velocidade_media +
+                ", velocidade_maxima=" + velocidade_maxima +
+                ", data_treino=" + data_treino +
+                ", duracao=" + duracao +
+                '}';
+    }
+}
--- app/app/src/main/java/com/psi/ciclodias/model/DBHelp.java
+++ app/app/src/main/java/com/psi/ciclodias/model/DBHelp.java
@@ -1,8 +1,14 @@
 package com.psi.ciclodias.model;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.time.LocalDate;
 
 public class DBHelp extends SQLiteOpenHelper {
     private static final String DB_NOME = "projectdb";
@@ -26,6 +32,10 @@ public class DBHelp extends SQLiteOpenHelper {
     private static final String ULTIMO_NOME = "ultimo_nome";
     private static final String DATA_NASCIMENTO = "data_nascimento";
     private static final String USER_ID_USERINFO = "user_id";
+
+    private static final String USERNAME = "username";
+    private static final String EMAIL = "email";
+    private static final String PASSWORD = "password";
 
     private SQLiteDatabase bd;
 
@@ -68,5 +78,120 @@ public class DBHelp extends SQLiteOpenHelper {
         db.execSQL(SQL1);
 
         this.onCreate(db);
+    }
+
+    // métodos CRUD
+    public Ciclismo AdicionarCiclismoDB(Ciclismo ciclismo){
+        ContentValues valores = new ContentValues();
+
+        valores.put(NOME_PERCURSO, ciclismo.getNome_percurso());
+        valores.put(DURACAO, "" + ciclismo.getDuracao());
+        valores.put(DISTANCIA, ciclismo.getDistancia());
+        valores.put(VELOCIDADE_MEDIA, ciclismo.getVelocidade_media());
+        valores.put(VELOCIDADE_MAXIMA, ciclismo.getVelocidade_maxima());
+        valores.put(VELOCIDADE_GRAFICO, ciclismo.getVelocidade_grafico());
+        valores.put(ROTA, ciclismo.getRota());
+        valores.put(DATA_TREINO, "" + ciclismo.getData_treino());
+
+        long id = bd.insert(TABELA_CICLISMO, null, valores);
+
+        // Se devolver -1 é porque não conseguiu inserir
+        if (id != -1){
+            ciclismo.setId(id);
+            return ciclismo;
+        }
+
+        return null;
+    }
+
+    public ArrayList<Ciclismo> getListaCiclismoDB(){
+        ArrayList<Ciclismo> lista = new ArrayList<>();
+
+        // Query á tabela toda
+        Cursor cursor = bd.query(TABELA_CICLISMO, new String[] {ID_CICLISMO, NOME_PERCURSO, DURACAO, DISTANCIA, VELOCIDADE_MEDIA,
+                        VELOCIDADE_MAXIMA, VELOCIDADE_GRAFICO, ROTA, DATA_TREINO, USER_ID_CICLISMO},
+                null, null, null, null, null);
+
+        // Se o encontrar algum faz
+        if(cursor.moveToFirst()){
+            // Faz enquanto ainda houve mais treinos
+            do {
+                Ciclismo aux = new Ciclismo(cursor.getString(0),
+                        // Tipo de data está em String por agora
+                        cursor.getString(1),
+                        // ---------------------------------------
+                        cursor.getFloat(2),
+                        cursor.getFloat(3),
+                        cursor.getFloat(4),
+                        cursor.getString(5),
+                        cursor.getString(6));
+                lista.add(aux);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        return lista;
+    }
+
+    public boolean editarCiclismoDB(Ciclismo ciclismo){
+        ContentValues valores = new ContentValues();
+
+        // O utilizadr apenas pode editar o nome do percurso
+        valores.put(NOME_PERCURSO, ciclismo.getNome_percurso());
+
+        // Retorna na Tabela Ciclismo o resultado de editar o treino com o ID enviado
+        return bd.update(TABELA_CICLISMO, valores, ID_CICLISMO + " = ?", new String[] {"" + ciclismo.getId()}) > 0;
+    }
+
+    public boolean apagarCiclismoDB(long id){
+        // Retorna na Tabela Ciclismo o resultado de apagar o treino com o ID enviado
+        return bd.delete(TABELA_CICLISMO, ID_CICLISMO + " = ?", new String[] {"" + id}) == 1;
+    }
+
+
+    // métodos CRUD
+    public User AdicionarUserDB(User user){
+        ContentValues valores = new ContentValues();
+
+        valores.put(USERNAME, user.getUsername());
+        valores.put(EMAIL, user.getEmail());
+        valores.put(PASSWORD, user.getPassword());
+        valores.put(PRIMEIRO_NOME, user.getPrimeiro_nome());
+        valores.put(ULTIMO_NOME, user.getUltimo_nome());
+
+        long id = bd.insert(TABELA_CICLISMO, null, valores);
+
+        // Se devolver -1 é porque não conseguiu inserir
+        if (id != -1){
+            user.setId(id);
+            return user;
+        }
+
+        return null;
+    }
+
+    public User getUser(){
+        User user;
+        // Query á tabela toda
+        Cursor cursor = bd.query(TABELA_CICLISMO, new String[] {USER_ID_USERINFO},
+                null, null, null, null, null);
+
+        // Se o encontrar algum faz
+        if(cursor.moveToFirst()){
+            // Faz enquanto ainda houve mais treinos
+            user = new User(cursor.getString(0),
+            cursor.getString(1),
+            // ---------------------------------------
+            cursor.getString(2),
+            cursor.getString(3),
+            cursor.getString(4));
+
+            cursor.close();
+
+            return user;
+        }
+
+        cursor.close();
+        return null;
     }
 }
