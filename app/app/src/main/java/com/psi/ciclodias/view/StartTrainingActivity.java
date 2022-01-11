cc3e0afa1fef8450785360107aa38af6972edd72
iuricarras <iuri.carrasqueiro@gmail.com>
2022-01-11 15:15:39 +0000

    Pedido das permissões corrigido

--- app/app/src/main/java/com/psi/ciclodias/view/StartTrainingActivity.java
+++ app/app/src/main/java/com/psi/ciclodias/view/StartTrainingActivity.java
@@ -1,12 +1,20 @@
 package com.psi.ciclodias.view;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.psi.ciclodias.R;
 import com.psi.ciclodias.databinding.ActivityStartTrainingBinding;
 
@@ -17,18 +25,24 @@ public class StartTrainingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Fragment mapfragment = mapFragment.getInstancia();
+            mapFragment.getInstancia().startBinding = binding;
+            if(mapfragment != null){
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mapViewStartTraining, mapfragment)
+                        .commit();
+            }
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+
         // Recebe os IDs da Activity Start Training
         binding = ActivityStartTrainingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Fragment mapfragment = mapFragment.getInstancia();
-        mapFragment.getInstancia().startBinding = binding;
-        if(mapfragment != null){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.mapViewStartTraining, mapfragment)
-                    .commit();
-        }
 
         // ----------------------- Inicio da Bottom-navbar --------------------------------
         BottomNavBarFragment fragment = new BottomNavBarFragment();
@@ -51,5 +65,26 @@ public class StartTrainingActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    System.out.println("henlo permission");
+                    Fragment mapfragment = mapFragment.getInstancia();
+                    mapFragment.getInstancia().startBinding = binding;
+                    if (mapfragment != null) {
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.mapViewStartTraining, mapfragment)
+                                .commit();
+                    }
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), MainPageActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            });
 }
